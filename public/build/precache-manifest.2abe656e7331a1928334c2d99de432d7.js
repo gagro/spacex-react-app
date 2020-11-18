68f68383cab00bf68431fbf2b4dc06ae3ba6d693
Toni Gagro <toni.gagro@gmail.com>
2020-11-18 13:37:47 +0100

    Testing heroku build

---
+++ public/build/precache-manifest.2abe656e7331a1928334c2d99de432d7.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94303b879cdd61fad52c3efee5a16621",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2646361ce0120e600c5e",
+    "url": "/static/css/main.e75dfb78.chunk.css"
+  },
+  {
+    "revision": "65db3799a359c0f494bb",
+    "url": "/static/js/2.8cd7cb28.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.8cd7cb28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e012f889234e7b3131",
+    "url": "/static/js/3.10bb76e3.chunk.js"
+  },
+  {
+    "revision": "51297ddbeb010e15feee",
+    "url": "/static/js/4.f10608c9.chunk.js"
+  },
+  {
+    "revision": "0f38c26783fbc265125b",
+    "url": "/static/js/5.7fab0efe.chunk.js"
+  },
+  {
+    "revision": "212ebde92bab68b4dcbf",
+    "url": "/static/js/6.6176b1b7.chunk.js"
+  },
+  {
+    "revision": "4930b036ee14e3c25d19",
+    "url": "/static/js/7.a1d8b76e.chunk.js"
+  },
+  {
+    "revision": "2646361ce0120e600c5e",
+    "url": "/static/js/main.56800e70.chunk.js"
+  },
+  {
+    "revision": "5865cfbdd3cad1081e50",
+    "url": "/static/js/runtime-main.77eb0b9d.js"
+  },
+  {
+    "revision": "ac2c4ed3f4204ac5049309edc58fb0c3",
+    "url": "/static/media/BGMobile.ac2c4ed3.svg"
+  },
+  {
+    "revision": "a816fad90574f4144f69027b9094abbc",
+    "url": "/static/media/github.a816fad9.svg"
+  },
+  {
+    "revision": "c68750c1786e0667997543b2e525a0ac",
+    "url": "/static/media/home.c68750c1.svg"
+  },
+  {
+    "revision": "f1d59c48b7d78c362ee58af8e5c35d3b",
+    "url": "/static/media/launches.f1d59c48.svg"
+  },
+  {
+    "revision": "0946d1b8151e852a65d16047fb929e20",
+    "url": "/static/media/left-arrow.0946d1b8.svg"
+  },
+  {
+    "revision": "2f1a856f6036db5be1cefe7545a69fbb",
+    "url": "/static/media/linkedin.2f1a856f.svg"
+  },
+  {
+    "revision": "290061bdf305769519acd12749b2fb29",
+    "url": "/static/media/missions.290061bd.svg"
+  },
+  {
+    "revision": "df33b1df35c35186df6ad64094aeab3a",
+    "url": "/static/media/right-arrow.df33b1df.svg"
+  },
+  {
+    "revision": "83fb2969aad60964e3e343f2c034727f",
+    "url": "/static/media/rockets.83fb2969.svg"
+  },
+  {
+    "revision": "a4750d476a1842f8639b1d2e297d3005",
+    "url": "/static/media/ships.a4750d47.svg"
+  },
+  {
+    "revision": "9379f21b255ee72bb50e7cf76c8cb735",
+    "url": "/static/media/twitter.9379f21b.svg"
+  },
+  {
+    "revision": "d68dac0353fde6da0cf8662765888cb4",
+    "url": "/static/media/wikipedia.d68dac03.svg"
+  },
+  {
+    "revision": "43e9d8b83cb24f51e1f11708c317e928",
+    "url": "/static/media/www.43e9d8b8.svg"
+  },
+  {
+    "revision": "6a6de51799ed09b21a302ce066be4a8e",
+    "url": "/static/media/youtube.6a6de517.svg"
+  }
+]);
